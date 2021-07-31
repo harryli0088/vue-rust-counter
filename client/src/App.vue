62f201bcf114ac3b0ebc4e931ce5760005595616
harryli0088 <harryli0088@gmail.com>
2021-07-31 00:18:38 -0400

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <Todo msg="Welcome to this Vue.js + TypeScript Todo App"/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Todo from './components/Todo.vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Todo
+  }
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/Todo.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <form v-on:submit="create">
+      <input
+        @change="value=$event.target.value"
+        v-bind:value="value"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+type State = {
+  value: string,
+}
+export default defineComponent({
+  data: ():State => ({
+    value: "",
+  }),
+  methods: {
+    create: function(e: Event) {
+      e.preventDefault()
+      console.log("this.value",this.value)
+    },
+    delete: function() {
+      console.log("this.value",this.value)
+
+    },
+    update: function() {
+      console.log("this.value",this.value)
+
+    },
+  },
+  name: 'Todo',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
