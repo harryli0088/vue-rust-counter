71580cfe7982ba0325193a0ca66e0301355604e0
harryli0088 <harryli0088@gmail.com>
2021-08-03 19:12:07 -0400

    working deploy, Vue docs are stupid

---
+++ client/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+# Vue's docs are stupid. I had to replace the SSH URL with HTTPS URL and replace 'main' with 'master'
+git push -f https://github.com/harryli0088/vue-rust-counter.git master:gh-pages
+
+cd -
